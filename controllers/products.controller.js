1493a44812e26f0507b57fa9d3a3007e2da28e1c
Madaliev Samandar <samandarmadaliev@yandex.com>
2023-07-27 22:49:29 +0500

    added a controller and models

---
+++ controllers/products.controller.js
@@ -0,0 +1,29 @@
+const Product = require("../models/products.models");
+
+async function getProducts(req, res) {
+  try {
+    const products = await Product.findAll();
+    res.writeHead(200, { "Content-Type": "application-json" });
+    res.end(JSON.stringify(products));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getProduct(req, res, id){
+  try {
+    const product = await Product.findById(id);
+    if(!product){
+      res.writeHead(400, { "Content-Type": "application-json" });
+      res.end(JSON.stringify({message: "Product Not Found"}));
+    }else{
+      res.writeHead(200, { "Content-Type": "application-json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
+module.exports = { getProducts, getProduct}
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const http = require("http");
-const products = require("./data/products.json");
-const server = http.createServer((req, res) =>{
-  if(req.url === "/api/products"){
-    res.writeHead(200, {"Content-Type":"application-json"});
-    res.end(JSON.stringify(products));
-  }else{
-    res.writeHead(404, {"Content-Type":"application-json"});
-    res.end(JSON.stringify({error:"Not Found"}));
+const { getProducts, getProduct } = require("./controllers/products.controller");
+const server = http.createServer((req, res) => {
+  if (req.url === "/api/products" && req.method === "GET") {
+    getProducts(req, res);
+  } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === "GET"){
+    const id = req.url.split("/")[3];
+    getProduct(req, res, id)
+  } 
+  else {
+    res.writeHead(404, { "Content-Type": "application-json" });
+    res.end(JSON.stringify({ error: "Not Found" }));
   }
 });
 
 const PORT = process.env.PORT || 7000;
 
-server.listen(PORT, ()=>console.log(`>${PORT}`));
+server.listen(PORT, () => console.log(`PORT:${PORT}`));
